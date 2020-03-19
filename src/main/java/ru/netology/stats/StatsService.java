24ce89a9defe084a821aa6b15261a87d9b5f0396
AntonZ-Frank <toni.rus@yandex.ru>
2020-03-19 12:54:44 +0500

    reformate code

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -1,34 +1,37 @@
 package ru.netology.stats;
 
 public class StatsService {
-    public static int calculateSumAndMidelSum(int[] salesInfos)  {
-    int sum = 0;
-    for (int salesInfo : salesInfos) {
-        sum += salesInfo;
-    }
+    public static int calculateSumAndMidelSum(int[] salesInfos) {
+        int sum = 0;
+        for (int salesInfo : salesInfos) {
+            sum += salesInfo;
+        }
         return sum;
-     }
+    }
 
     public static int calculatelowMonth(int[] salesInfos, int midelsum) {
-           int lowMonth = 0;
-       for (int salesInfo : salesInfos) {
-           if ( salesInfo < midelsum)
-           {lowMonth += 1;}
-       }
+        int lowMonth = 0;
+        for (int salesInfo : salesInfos) {
+            if (salesInfo < midelsum) {
+                lowMonth += 1;
+            }
+        }
         return lowMonth;
     }
 
     public static int calculatehighMonth(int[] salesInfos, int midelsum) {
-                int highMonth = 0;
+        int highMonth = 0;
         for (int salesInfo : salesInfos) {
-            if ( salesInfo > midelsum)
-            {highMonth += 1;}
+            if (salesInfo > midelsum) {
+                highMonth += 1;
+            }
         }
         return highMonth;
     }
+
     public static int calculateHowHighPayment(int[] salesInfos) {
         int highPayment = 0;
-       int max = salesInfos[0];
+        int max = salesInfos[0];
         for (int i = 0; i < salesInfos.length; i++) {
             int salesInfo = salesInfos[i];
             if (max <= salesInfo) {
@@ -38,6 +41,7 @@ public class StatsService {
         }
         return highPayment + 1; //Прибавляем 1 т.к. #месяца начинается с 1го, а массив с 0.
     }
+
     public static int calculateHowLowPayment(int[] salesInfos) {
 
         int lowPayment = 0;
